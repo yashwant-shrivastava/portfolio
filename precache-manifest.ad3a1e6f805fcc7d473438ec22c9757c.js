32841cab144562adca30f412b2d8ad039da323c3
Yashwant Shrivastava <yashwant.shrivastava@zomato.com>
2020-08-02 21:51:31 +0530

    Updates

---
+++ precache-manifest.ad3a1e6f805fcc7d473438ec22c9757c.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b09cbe9a49fd3c5e22dc425fe2c8579",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d738ed33589400915471",
+    "url": "/portfolio/static/css/main.eb397c5d.chunk.css"
+  },
+  {
+    "revision": "3403e43c9dc43c9f42d4",
+    "url": "/portfolio/static/js/2.f4e68f3c.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/portfolio/static/js/2.f4e68f3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d738ed33589400915471",
+    "url": "/portfolio/static/js/main.7587e5f6.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/portfolio/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "a028649ddc58bd148351afa25dda8f23",
+    "url": "/portfolio/static/media/c++.a028649d.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/portfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "2208ef3493476209e154234bb39974b0",
+    "url": "/portfolio/static/media/docker.2208ef34.svg"
+  },
+  {
+    "revision": "e510c5b0fe6b6bd3a789f9614ecbf944",
+    "url": "/portfolio/static/media/email.e510c5b0.svg"
+  },
+  {
+    "revision": "d98463abd102acdc2b1eb8d5eb70b2d2",
+    "url": "/portfolio/static/media/flask.d98463ab.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/portfolio/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/portfolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "4fcf40b67a0ae4f435d198214fd0e5a1",
+    "url": "/portfolio/static/media/img1.4fcf40b6.jpg"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/portfolio/static/media/img3.7256f309.webp"
+  },
+  {
+    "revision": "ca17dac1156c582e535684b016dbc0f3",
+    "url": "/portfolio/static/media/img3.ca17dac1.jpg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/portfolio/static/media/img4.8bce7aef.webp"
+  },
+  {
+    "revision": "13a096531e50277a3fb17460c39db221",
+    "url": "/portfolio/static/media/java.13a09653.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/portfolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "7e1c9f907cdcf0e63140cc9db348318b",
+    "url": "/portfolio/static/media/jquery.7e1c9f90.svg"
+  },
+  {
+    "revision": "a0fb441744ffa7fc183dd749fedce9fc",
+    "url": "/portfolio/static/media/logo.a0fb4417.png"
+  },
+  {
+    "revision": "cfee114b0b3de28827b98bee2ac0d6f0",
+    "url": "/portfolio/static/media/mongo.cfee114b.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/portfolio/static/media/mysql.54d7bddc.svg"
+  },
+  {
+    "revision": "b7a7755cc48fa450b0265ec65c6115a8",
+    "url": "/portfolio/static/media/phone.b7a7755c.svg"
+  },
+  {
+    "revision": "b8dd4f064495e994c886b86e0cd2363e",
+    "url": "/portfolio/static/media/php.b8dd4f06.svg"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/portfolio/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/portfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "841d4ad6e0697c2c707115e8f4ce5ec5",
+    "url": "/portfolio/static/media/redis.841d4ad6.svg"
+  },
+  {
+    "revision": "594e5be420557bfcf2266ef02424fa54",
+    "url": "/portfolio/static/media/terraform.594e5be4.svg"
+  },
+  {
+    "revision": "9b6e2a11298d9f7a9bc867566728d49f",
+    "url": "/portfolio/static/media/yashwant.9b6e2a11.jpg"
+  },
+  {
+    "revision": "564522da2fa3cb83931971ac18f3eeda",
+    "url": "/portfolio/static/media/zomato.564522da.svg"
+  }
+]);
